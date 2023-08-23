fa6ad2908a9f2039d016fa1b38465e3d8d76969a
Yukinit-7272 <140945000+Yukinit-7272@users.noreply.github.com>
2023-08-23 21:58:43 +0900

    Update result.php

--- 3-3/result.php
+++ 3-3/result.php
@@ -2,8 +2,7 @@
 $my_name = $_GET['my_name'];
 $number = $_GET['number'];
 
-$rand = mt_rand(1,6);
-$result = getTotal($rand, $number);
+$result = $number * mt_rand(1,6);
 
 if ($result >= 1 && $result <=10) {
     $omikuji = "凶";
@@ -19,9 +18,7 @@ if ($result >= 1 && $result <=10) {
     $omikuji = "残念";
 }
 
-function getTotal($rand, $number) {
-    return $rand * $number;
-}
+date_default_timezone_set('Asia/Tokyo');
 ?>
 
 <p><?php echo date("Y-m-d H:i:s", time()); ?></p>
