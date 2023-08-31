887488378244c3115b345f5a015379271ce67a58
Yukinit-7272 <140945000+Yukinit-7272@users.noreply.github.com>
2023-08-31 21:57:05 +0900

    Update answer.php

--- 3-4/answer.php
+++ 3-4/answer.php
@@ -13,37 +13,35 @@
     ?>
     <p><?php echo $my_name; ?>さんの結果は・・・？</p>
 
+    <?php
+    function getAnswer ($answer, $selectedans){
+      if ($answer == $selectedans) {
+        echo "正解！";
+        } else {
+        echo "残念・・・";
+        }
+    }
+    ?>
+
     <!-- 質問1回答 -->
     <p style="margin: 20px 0;">①の答え</p>
     <?php
-      $seikai1 = $_POST['ans1'];
-      if($seikai1 == 80) {
-      echo "正解！";
-      } else {
-      echo "残念・・・";
-      }
+      $result1 = $_POST['ans1'];
+      getAnswer (80, $result1);
     ?>
 
     <!-- 質問2回答 -->
     <p style="margin: 20px 0;">②の答え</p>
     <?php
-      $seikai2 = $_POST['ans2'];
-      if($seikai2 == "HTML") {
-      echo "正解！";
-      } else {
-      echo "残念・・・";
-      }
+      $result2 = $_POST['ans2'];
+      getAnswer ("HTML", $result2);
     ?>
 
     <!-- 質問3回答 -->
     <p style="margin: 20px 0;">③の答え</p>
     <?php
-      $seikai3 = $_POST['ans3'];
-      if($seikai3 == "select") {
-      echo "正解！";
-      } else {
-      echo "残念・・・";
-      }
-    ?>
+      $result3 = $_POST['ans3'];
+      getAnswer ("select", $result3);
+     ?>
 </body>
 </html>
